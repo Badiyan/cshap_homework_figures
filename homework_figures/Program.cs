9c8b503553311ef1f1fe13786bbb9a76b4fa991f
Badiyan <e.mailbox.sbv@gmail.com>
2022-07-18 20:08:35 +0300

    1st work variant (straight triangle using for)

--- homework_figures/Program.cs
+++ homework_figures/Program.cs
@@ -10,6 +10,17 @@ namespace homework_figures
     {
         static void Main(string[] args)
         {
+            #region - triangle 1st variant
+
+        
+            string oneBlockOfTriangle = "";
+            for (string asterix = "*"; oneBlockOfTriangle.Length < 10; oneBlockOfTriangle = string.Concat(oneBlockOfTriangle, asterix))
+            {
+                Console.WriteLine(oneBlockOfTriangle);
+            }
+            Console.ReadKey();
+
+            #endregion
 
         }
     }
